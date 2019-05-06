d3cef96730f9ece8a2d9c1be7768ecb081e88102
hajuhyun <mangchhe@naver.com>
2019-05-06 22:17:55 +0900

    Update Server&Client Communication

--- src/tcp_ip_c/ClientBackground.java
+++ src/tcp_ip_c/ClientBackground.java
@@ -2,10 +2,17 @@ package tcp_ip_c;
 
 import java.net.Socket;
 import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Scanner;
 
 public class ClientBackground {
     
     Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+    Scanner sc = new Scanner(System.in);
+    String msg;
     
     public void connect(){
         
@@ -13,6 +20,14 @@ public class ClientBackground {
             
             socket = new Socket("192.168.0.7",7777);
             System.out.println("접속에 성공하였습니다.");
+            
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            while(out!=null){
+                System.out.println("서버님에 말씀 : "+in.readUTF());
+                //out.writeUTF(sc.nextLine());
+            }
+        
             
         }catch(IOException e){
             e.printStackTrace();
--- src/tcp_ip_s/ServerBackground.java
+++ src/tcp_ip_s/ServerBackground.java
@@ -3,21 +3,41 @@ package tcp_ip_s;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Scanner;
 
 public class ServerBackground {
     
     ServerSocket serverSocket;
     Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+    Scanner sc = new Scanner(System.in);
+    String msg;
     
     public void setting(){
         try{
             
-        serverSocket = new ServerSocket(7777);
-        System.out.println("접속 대기중");
-        socket=serverSocket.accept();
+            serverSocket = new ServerSocket(7777);
+            System.out.println("접속 대기중");
+            socket=serverSocket.accept();
+            System.out.println(socket.getInetAddress()+"님께서 접속하셨습니다.");
+        
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            
+            abc a=new abc(socket);
+            a.start();
+        
+            while(in!=null){
+                System.out.println("고객님 말씀 : "+in.readUTF());
+            //    out.writeUTF(sc.nextLine());
+            }
+        
         
         }catch(IOException e){
-            e.printStackTrace();
+            System.out.println(socket.getInetAddress()+"님께서 퇴장하셨습니다.");
         }
     }
     
@@ -26,6 +46,32 @@ public class ServerBackground {
         ServerBackground serverBackground = new ServerBackground();
         serverBackground.setting();
         
+    }
+    
+    class abc extends Thread{
+        
+       // DataInputStream in;
+        //DataOutputStream out;
+       // Socket socket;
+        
+        public abc(Socket socket){
+            try{
+                in = new DataInputStream(socket.getInputStream());
+                //out = new DataOutputStream(socket.getOutputStream());
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        
+        public void run(){
+            while(out!=null){
+                try{
+                    out.writeUTF(sc.nextLine());
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }    
+        }
     }
     
 }
